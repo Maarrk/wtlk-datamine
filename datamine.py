8f20bd554e08da81710ebabf4109b384d775ead9
Marek Ł <marqus34@gmail.com>
2017-11-21 18:23:53 +0100

    Serializing year and decade as integers

--- datamine.py
+++ datamine.py
@@ -20,6 +20,7 @@ planedata = {'planes':[]}
 pat_firstflight = re.compile(r'(First flight|Introduction)</th>\n<td>(.*?)<')
 
 for i in range(len(titles)):
+    plane = {'name': titles[i].replace('_', ' ')}
     try:
         with urllib.request.urlopen('https://en.wikipedia.org/wiki/' + titles[i]) as response:
             html = response.read().decode('utf-8')
@@ -29,23 +30,26 @@ for i in range(len(titles)):
                 firstflight = result.group(2)
                 decade = re.search(r'\d\d\d\ds', firstflight)
                 if decade:
-                    print(Fore.YELLOW + titles[i] + ' ' + decade.group()[:-1] + Fore.RESET)
+                    dec_num = int(decade.group()[:-1])
+                    print(Fore.YELLOW + titles[i] + ' ' + str(dec_num) + Fore.RESET)
+                    plane['decade'] = dec_num
                 else:
                     year = re.search(r'\d\d\d\d', firstflight)
                     if year:
-                        print(Fore.GREEN + titles[i] + ' ' + year.group() + Fore.RESET)
+                        y_num = int(year.group())
+                        print(Fore.GREEN + titles[i] + ' ' + str(y_num) + Fore.RESET)
+                        plane['year'] = y_num
+                        plane['decade'] = y_num - y_num % 10
                     else:
                         print(Fore.RED + titles[i] + ' has no First flight / Introduction year or decade' + Fore.RESET)
-
-                plane = {'name': titles[i].replace('_', ' '), 'firstflight': result.group(2)}
-                planedata['planes'].append(plane)
-                planefile = open('planes.json', 'w')
-                # json.dump(planedata, planefile)
-                planefile.write(json.dumps(planedata, indent=4))
-                planefile.close()
             else:
                 print(Fore.RED + titles[i] + ' has no First flight / Introduction data' + Fore.RESET)
     except UnicodeEncodeError:
-            print(Fore.RED + titles[i] + ' has bad encoding' + Fore.RESET)
-            continue
+        print(Fore.RED + titles[i] + ' has bad encoding' + Fore.RESET)
+        plane['error'] = 'UnicodeEncodeError'
+
+    planedata['planes'].append(plane)
+    planefile = open('planes.json', 'w')
+    planefile.write(json.dumps(planedata, indent=4))
+    planefile.close()
 
