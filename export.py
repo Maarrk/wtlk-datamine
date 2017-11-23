942b712c6dd7eea089748e2fad16a50eb29628ca
Marek Ł <marqus34@gmail.com>
2017-11-23 14:51:17 +0100

    Exporting to spreadsheet-readable txt

---
+++ export.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# pylint: disable=C0103
+
+import json
+from copy import copy
+
+planefile = open('planes_all.json', 'r')
+planedata = json.load(planefile)
+planefile.close()
+
+columns = ['name', 'year', 'decade', 'crew', 'capacity', 'length', 'wingspan', 'emptyweight']
+row = []
+
+with open('planes_export.txt', 'w') as txt_file:
+    text = '; '.join([str(cell) for cell in columns]) + '\n'
+    txt_file.write(text)
+
+    for pln in planedata['planes']:
+        row.clear()
+        for col in columns:
+            if col in pln:
+                row.append(pln[col])
+            else:
+                row.append('UNKNOWN')
+
+        text = '; '.join([str(cell) for cell in row]) + '\n'
+        txt_file.write(text)
